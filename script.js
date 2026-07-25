d41a1bda3213f7b26feff87a881eb7459227a3e7
Fiuzah <isabelle.fiuza@escola.pr.gov.br>
2026-07-25 19:17:17 -0300

    dados simulados ADD

---
+++ script.js
@@ -0,0 +1,9 @@
+let sensores = {
+    temperatura:26,
+    umidadeAr:68,
+    umidadeSolo:35,
+    luminosidade:"Baixa",
+    qualidadeAgua:"Adequada",
+    nivelAgua:"Normal",
+    composteira:"Pronta"
+};
